7b9579f31447c61cd191a8645f5d061870849f75
Vlad Yahnovets <v.yahnavets@gmail.com>
2025-03-09 17:25:08 +0000

    Finish program

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#include <stdio.h>
-#include <wchar.h>
-
 // Функция для нахождения последнего вхождения буквы в строку
 int find_last_occurrence(const wchar_t* str, wchar_t letter) {
 		int last_index = -1; // Изначально нет вхождения
@@ -16,6 +13,29 @@ int find_last_occurrence(const wchar_t* str, wchar_t letter) {
 		return last_index;
 }
 
+// Функция для сравнения двух позиций
+int compare_two_positions(int pos1, int pos2) {
+		if (pos1 > pos2) {
+				return 1; // Позиция в первом предложении больше
+		} else if (pos2 > pos1) {
+				return -1; // Позиция во втором предложении больше
+		} else {
+				return 0; // Позиции одинаковые
+		}
+}
+
+// Функция для вывода результата сравнения позиций
+void compare_positions(int pos1, int pos2) {
+		int result = compare_two_positions(pos1, pos2);
+		if (result == 1) {
+				printf("В первом предложении буква 'ш' имеет больший порядковый номер.\n");
+		} else if (result == -1) {
+				printf("Во втором предложении буква 'ш' имеет больший порядковый номер.\n");
+		} else {
+				printf("В обоих предложениях буква 'ш' имеет одинаковый порядковый номер.\n");
+		}
+}
+
 int main() {
 		wchar_t sentence1[] = L"Я люблю программировать на C.";
 		wchar_t sentence2[] = L"Школа была далеко, но мы пришли вовремя.";
@@ -24,9 +44,8 @@ int main() {
 		int last_sh1 = find_last_occurrence(sentence1, L'ш');
 		int last_sh2 = find_last_occurrence(sentence2, L'ш');
 
-		// Выводим позиции последнего вхождения буквы "ш"
-		printf("Последняя позиция буквы 'ш' в первом предложении: %d\n", last_sh1);
-		printf("Последняя позиция буквы 'ш' во втором предложении: %d\n", last_sh2);
+	// Сравниваем позиции
+	compare_positions(last_sh1, last_sh2);
 
 		return 0;
 }
